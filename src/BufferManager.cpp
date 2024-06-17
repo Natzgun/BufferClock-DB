8affe446145badac4347b4e32723bf9f3ea34db6
Natzgun <itzlilouxx@gmail.com>
2024-06-16 22:52:26 -0500

    Dummy conditional is commented XD

--- src/BufferManager.cpp
+++ src/BufferManager.cpp
@@ -83,10 +83,12 @@ void BufferManager ::useClockPolicy(int pageID, string path, char _mode) {
 }
 
 void BufferManager::killProcess(int pageID) {
+  /*
   if (bpool.getFrames()[bpool.getFrameId(pageID)].getPinned() == true) {
     cout << "Hey, hey, hey... Esta pagina esta PINEADA no la puedes tocar \n";
     return;
   }
+  */
 
   std::string killed;
   bpool.getFrames()[bpool.getFrameId(pageID)].showVector();
